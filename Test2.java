1166af1a3c9f74a970d63f5f411eb9c534d179f3
Yuvaraj_Ajay <yuvarajk.official1@gmail.com>
2025-11-10 22:01:52 +0530

    Files added

--- Test2.java
+++ Test2.java
@@ -1,1 +1,41 @@
+/* 
+Write a Java program to print the following patterns:
+
+11111                       1   
+22222                       21  
+33333                       321 
+44444                       4321
+55555                       54321
+
+
+for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+*/
+
+
+public class Test2
+{
+    public static void main(String []args)
+    {
+        
+        for (int i = 1; i <= 4; i++)
+        {
+            for (int j = i; j >= 0; j--)
+            {
+                    System.out.print(j);
+            }
+            System.out.println();
+        }
+        
+    }
+}
+
+
 
---
+++ Test3.java
@@ -0,0 +1,31 @@
+/*
+ 
+Write a Java program to print the following patterns:
+
+    1                       1   
+   21                       21  
+  321                       321 
+ 4321                       4321
+54321                       54321
+
+
+ */
+
+
+
+public class Test3 {
+    public static void main(String []args)
+    {
+       for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 5; j >= 1; j--)
+            {
+                if (j <= i)    
+                    System.out.print(j);
+                else   
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }    
+}
