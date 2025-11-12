ab60528a42181c1bc175bf0ae8168435ed26435d
Yuvaraj_Ajay <yuvarajk.official1@gmail.com>
2025-11-12 09:59:00 +0530

    File modified

--- Test6.java
+++ Test6.java
@@ -8,7 +8,7 @@ public class Test6 {
      *     1
      * */
     public static void main(String[] args) {
-        int rows = 5;
+        
         for (int i = 5; i >= 1; i--) {
             for (int j = 0; j < 5 - i; j++)
                 System.out.print(".");
