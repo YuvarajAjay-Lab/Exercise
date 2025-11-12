7472ba22841f48a02f16dc73e74e095c94abc280
Gokul-Da <gokul.tn.ind@gmail.com>
2025-11-12 09:33:35 +0530

    Pattern- Pyramid and reverse Pyramid

---
+++ Test5.java
@@ -0,0 +1,55 @@
+public class Test5 {
+    /*
+     *    1
+     *   111
+     *  11111
+     * 1111111*/
+    //
+//    1
+//   111
+//  11111
+// 1111111
+//111111111
+    public static void main(String[] args) {
+        gptAnswer();
+
+    }
+
+   static void gokulAnswer(){
+        int maxOddNum = 9;
+        int positionCounter = 5;
+        for (int i = 1; i <= maxOddNum; i = i + 2) {
+            int charPrinted = 0;
+            for (int j = 1; j <= maxOddNum; j++) {
+                if(positionCounter<=j) {
+                    System.out.print(1);
+                    charPrinted++;
+                }
+                else
+                    System.out.print(" ");
+                if(charPrinted==i)
+                    break;
+            }
+            --positionCounter;
+            System.out.println();
+        }
+    }
+
+    static void gptAnswer(){
+        int rows = 5; // There are 5 lines in your example
+
+        for (int i = 1; i <= rows; i++) {
+            // Print leading spaces
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            // Print ones
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("1");
+            }
+            // Move to the next line
+            System.out.println();
+        }
+    }
+
+}
---
+++ Test6.java
@@ -0,0 +1,22 @@
+public class Test6 {
+    /*
+     * Print this:
+     * 111111111
+     *  1111111
+     *   11111
+     *    111
+     *     1
+     * */
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 0; j < 5 - i; j++)
+                System.out.print(".");
+            for (int k = 1; k<=2*i-1; k++) {
+                System.out.print(1);
+            }
+            System.out.println();
+        }
+
+    }
+}
