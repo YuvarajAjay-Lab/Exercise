18e9122cef3fc9894bcc4eee161e5f8f395e7e3b
Yuvaraj_Ajay <yuvarajk.official1@gmail.com>
2025-11-13 11:03:22 +0530

    File modified

--- Test6.java
+++ Test6.java
@@ -1,18 +1,25 @@
-public class Test6 {
-    /*
-     * Print this:
-     * 111111111
-     *  1111111
-     *   11111
-     *    111
-     *     1
-     * */
+/* Write a program to print the below pattern:
+
+111111111
+ 1111111
+  11111
+   111
+    1
+
+*/
+
+public class Test6 
+{
+    
     public static void main(String[] args) {
         
-        for (int i = 5; i >= 1; i--) {
+        for (int i = 5; i >= 1; i--) 
+        {
             for (int j = 0; j < 5 - i; j++)
                 System.out.print(".");
-            for (int k = 1; k<=2*i-1; k++) {
+
+            for (int k = 1; k<=2*i-1; k++) 
+            {
                 System.out.print(1);
             }
             System.out.println();
