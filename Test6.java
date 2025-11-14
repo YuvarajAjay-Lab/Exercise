0c81b445743861cee1df88cd2709a8a6aa54ca55
Yuvaraj_Ajay <yuvarajk.official1@gmail.com>
2025-11-14 08:46:03 +0530

    Test6.java file modified

--- Test6.java
+++ Test6.java
@@ -12,7 +12,7 @@ public class Test6
 {
     
     public static void main(String[] args) {
-        
+        System.out.println();
         for (int i = 5; i >= 1; i--) 
         {
             for (int j = 0; j < 5 - i; j++)
